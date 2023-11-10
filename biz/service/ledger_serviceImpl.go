c0629d3500484cf68c01991bb08fac3a35bbf982
XZ0730 <1055275260@qq.com>
2023-11-10 15:36:49 +0800

    fix: superfluous code

--- biz/service/ledger_serviceImpl.go
+++ biz/service/ledger_serviceImpl.go
@@ -1,11 +1,12 @@
 package service
 
 import (
+	"time"
+
 	"github.com/XZ0730/runFzu/biz/dal/db"
 	"github.com/XZ0730/runFzu/biz/model/ledger"
 	"github.com/XZ0730/runFzu/pkg/errno"
 	"github.com/cloudwego/kitex/pkg/klog"
-	"time"
 )
 
 func (l *LedgerService) CreateLedger(user_id int64, req *ledger.LedgerModel) (code int64, msg string) {
@@ -15,23 +16,11 @@ func (l *LedgerService) CreateLedger(user_id int64, req *ledger.LedgerModel) (co
 		klog.Error("[newLedger] error:", err.Error())
 		return errno.TimeError.ErrorCode, errno.TimeError.ErrorMsg
 	}
-	_, err = time.Parse(time.DateTime, req.GetCreateTime())
-	if err != nil {
-		klog.Error("[newLedger] error:", err.Error())
-		return errno.TimeError.ErrorCode, errno.TimeError.ErrorMsg
-	}
-
 	u_time, err := time.Parse(time.DateTime, req.GetCreateTime())
 	if err != nil {
 		klog.Error("[newLedger] error:", err.Error())
 		return errno.TimeError.ErrorCode, errno.TimeError.ErrorMsg
 	}
-	_, err = time.Parse(time.DateTime, req.GetCreateTime())
-	if err != nil {
-		klog.Error("[newLedger] error:", err.Error())
-		return errno.TimeError.ErrorCode, errno.TimeError.ErrorMsg
-	}
-
 	newLedger := db.NewLedger(req.GetLedgerId(), user_id, req.GetLedgerName(), req.GetCover(), c_time, u_time)
 	if err := db.CreateLedger(newLedger); err != nil {
 		klog.Error("[newLedger]create error:", err.Error())
